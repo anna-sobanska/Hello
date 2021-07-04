9c88183d953c2ae9e7c927aaa7cabedc508f942f
Anna Sobanska <ania.webdev@gmail.com>
2021-07-04 16:04:34 +0200

    use .back() method from string library

--- Hello/enterName.cpp
+++ Hello/enterName.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 
 void enterName() {
 	std::string name;
 	std::cout << "Enter name: ";
 	std::cin >> name;
 
-	if (name[name.length()-1] == 'a') {
+	if (name.back() == 'a') {
 		std::cout << "You are a woman!" << std::endl;
 	}
 	else {
